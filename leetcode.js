a606f43eca6ac349acba04fbb7e073e29ad29084
Riyad Ahsan <riyad.ahsan4516@gmail.com>
2022-10-19 18:13:02 +0600

    Solved the Best Time to Buy and Sell stock problem

--- leetcode.js
+++ leetcode.js
@@ -781,4 +781,16 @@ var intersect = function (nums1, nums2) {
 //-----------------------------------------------------------------------------------------------------------------------
 
 // 121. Best Time to Buy and Sell Stock
-// (not yet solved)
+
+var maxProfit = function (prices) {
+  if (!prices) return 0;
+  let buy = prices[0];
+  let profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    buy = Math.min(buy, prices[i]);
+    profit = Math.max(profit, prices[i] - buy);
+  }
+  return profit;
+};
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
